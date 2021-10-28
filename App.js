0991c94e9163d9c048297b6e64b42403b76e4b0a
wilyyy <alvarez_william@outlook.com>
2021-10-27 21:27:31 -0700

    added components in storybook

--- App.js
+++ App.js
@@ -3,14 +3,16 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LinkCompassCard from './comps/LinkCompassCard';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <LinkCompassCard />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export {default} from './storybook';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <LinkCompassCard />
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
