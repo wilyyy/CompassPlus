1b1567c196ee4bac5181f040779075c364d02712
wilyyy <alvarez_william@outlook.com>
2021-11-02 00:28:26 -0700

    used constant colors in my comps, made screens folder

---
+++ screens/SignUp/pickDestinations.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { COLORS } from '../../constants/styles.js';
+
+
+const windowWidth = Dimensions.get('window').width;
+console.log(windowWidth);
+const windowHeight = Dimensions.get('window').height;
+
+const 
+
+const PickDestinations = () => {
+
+}
+
+/*
+4 state for confirm select : home, school, work, other
+if corresponding checkbox selected = return appropriate screens
+up to 4 screens
+*/
--- storybook/stories/Screens/Screens.stories.js
+++
@@ -1,7 +0,0 @@
-import { action } from '@storybook/addon-actions';
-import { text } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/react-native';
-import React from 'react';
-import { Text } from 'react-native';
-import Button from '.';
-import CenterView from '../CenterView';
