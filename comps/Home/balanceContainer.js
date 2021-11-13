4c24ec17cbe76d5429d07a109e97c3278d217392
wilyyy <alvarez_william@outlook.com>
2021-11-12 21:26:26 -0800

    reverted a couple commits backward due to master branch error

--- comps/Home/balanceContainer.js
+++ comps/Home/balanceContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled from 'styled-components/native';
 
 export default function BalanceContainer({
     CardNumber="",
@@ -26,37 +26,37 @@ export default function BalanceContainer({
 }
 
 const Container = styled.View`
-    backgroundColor: #ffbb00;
+    background-color: #ffbb00;
     flex-direction: row;
     width: 318px;
     justify-content: space-between;
-`
+`;
 
 const TextCont = styled.View`
     flex-direction: column;
     margin-right: 24px;
     margin-left: 16px;
-`
+`;
 const TextTitle = styled.Text`
     font-weight: 700;
     color: #009DDC;
     font-size: 24px;
     margin-top: 8px;
-`
+`;
 
 const TextCard = styled.Text`
     font-size: 12px;
-`
+`;
 
 const BalanceCont = styled.TouchableOpacity`
-    backgroundColor: #252B42;
+    background-color: #252B42;
     padding: 16px;
     border-radius: 10px;
-`
+`;
 
 const BalanceText = styled.Text`
     color: #fff;
     font-size: 30px;
     font-weight: 700;
-`
+`;
 
