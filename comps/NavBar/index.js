50f74056175f0e6ee681818414a3b7a6045ca06b
Willy School <walvarez1@se14l0121-13.ad.bcit.ca>
2021-11-16 11:38:57 -0800

    added icons to nav bar

--- comps/NavBar/index.js
+++ comps/NavBar/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { View, Dimensions, StyleSheet, Text } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import styled from "styled-components/native";
+import { Icon } from 'react-native-elements'
+
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useNavigation } from '@react-navigation/native';
+import { COLORS } from '../../constants/styles';
+
 
 
 
@@ -19,12 +23,22 @@ const NavCont = styled.View`
     width: ${windowWidth}px;
     height:85px;
     flex-direction: row;
-    justify-content: space-evenly;
+    justify-content: space-between;
     align-items: center;
     background-color: #fff;
 `;
 
+const Section = styled.TouchableOpacity`
+    width: 25%;
+    height: 41px;
+    justify-content: center;
+    align-items: center;
+`;
 
+const NavText = styled.Text`
+    font-size: 11px;
+    color: ${COLORS.SPACECADET};
+`;
 
 export default function NavBar({
     navigation = useNavigation(),
@@ -32,18 +46,42 @@ export default function NavBar({
 
     return (
         <NavCont>
-            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-                <Text>Home</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate('MobileCard')}>
-                <Text>Card</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate('Map')}>
-                <Text>Map</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate('Account')}>
-                <Text>Account</Text>
-            </TouchableOpacity>
+            <Section onPress={() => navigation.navigate('Home')}>
+                <Icon
+                    name='home'
+                    type='entypo'
+                    color={COLORS.SPACECADET}
+                    size={35}
+                />
+                <NavText>Home</NavText>
+            </Section>
+            <Section onPress={() => navigation.navigate('MobileCard')}>
+                <Icon
+                    name='card'
+                    type='ionicon'
+                    color={COLORS.SPACECADET}
+                    size={35}
+                />
+                <NavText>Compass</NavText>
+            </Section>
+            <Section onPress={() => navigation.navigate('Map')}>
+                <Icon
+                    name='map-marker'
+                    type='font-awesome'
+                    color={COLORS.SPACECADET}
+                    size={35}
+                />
+                <NavText>Home</NavText>
+            </Section>
+            <Section onPress={() => navigation.navigate('Account')}>
+                <Icon
+                    name='account-circle'
+                    type='material'
+                    color={COLORS.SPACECADET}
+                    size={35}
+                />
+                <NavText>Home</NavText>
+            </Section>
         </NavCont>
     );
 }
