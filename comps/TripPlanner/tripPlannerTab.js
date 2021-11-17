404654ea1e304364bab526a16a8ef79cd1b37b82
wilyyy <alvarez_william@outlook.com>
2021-11-16 21:19:48 -0800

    remade tabs without scroll icons, add map next

--- comps/TripPlanner/tripPlannerTab.js
+++ comps/TripPlanner/tripPlannerTab.js
@@ -1,5 +1,5 @@
 import styled from "styled-components/native";
-import React from 'react';
+import React, { useState } from 'react';
 import { Tab, ThemeProvider } from 'react-native-elements';
 import { Dimensions, TouchableOpacity, SafeAreaView, ScrollView, StyleSheet, Text } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -27,40 +27,89 @@ const TabCont = styled.View`
 const BigTab = styled.TouchableOpacity`
     width: 50%;
     height: 47px;
-    background-color: ${COLORS.CAROLINABLUE};
+    background-color: ${props=>props.bigtab_color};
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
-
+    justify-content: center;
+    align-items: center;
 `;
 
 const SmallTab = styled.TouchableOpacity`
     width: 50%;
     height: 40px;
-    background-color: ${COLORS.SPACECADET};
+    background-color: ${props=>props.smtab_color};
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
+    justify-content: center;
+    align-items: center;
+`;
 
+const TabText = styled.Text`
+    font-size: 20px;
+    color: #fff;
+`;
+
+const SmTabText = styled.Text`
+    font-size: 16px;
+    color: #fff;
 `;
 
 const Display = styled.View`
     width: 100%;
     height: 142px;
     align-items: center;
-    background-color: #009DDC;
+    background-color: ${props=>props.bg_color};
 `;
 
-const TripPlannerTab = () => {
+const TripPlannerTab = ({
+    carolinablue = COLORS.CAROLINABLUE,
+    spacecadet = COLORS.SPACECADET
+}) => {
+    const [tabSwitch, setTabSwitch] = useState(false);
 
-    return (
-        <Container>
-            <TabCont>
-                <BigTab></BigTab>
-                <SmallTab></SmallTab>
-            </TabCont>
-            <Display>
-            </Display>
-        </Container>
-    );
+    const PressLeftTab = () => {
+        setTabSwitch(false);
+    }
+
+    const PressRightTab = () => {
+        setTabSwitch(true);
+    }
+    
+    if(tabSwitch === false){
+        return (
+            <Container>
+                <TabCont>
+                    <BigTab bigtab_color={carolinablue} onPress={PressLeftTab}>
+                        <TabText>Nearby Rides</TabText>
+                    </BigTab>
+                    <SmallTab smtab_color={spacecadet} onPress={PressRightTab}>
+                        <SmTabText>Saved Rides</SmTabText>
+                    </SmallTab>
+                </TabCont>
+                <Display bg_color={carolinablue}>
+                    <TabText>Nearbu Rides</TabText>
+                </Display>
+            </Container>
+        );
+    }
+
+    if(tabSwitch === true){
+        return (
+            <Container>
+                <TabCont>
+                    <SmallTab smtab_color={carolinablue} onPress={PressLeftTab}>
+                        <SmTabText>Nearby Rides</SmTabText>
+                    </SmallTab>
+                    <BigTab bigtab_color={spacecadet} onPress={PressRightTab}>
+                        <TabText>Saved Rides</TabText>
+                    </BigTab>
+                </TabCont>
+                <Display bg_color={spacecadet}>
+                    <TabText>Saved Rides</TabText>
+                </Display>
+            </Container>
+        );
+    }
 }
 
 export default TripPlannerTab;
