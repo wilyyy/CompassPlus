79fe793102c6b901ffb3f6f530d9bbe87ff61a6c
wilyyy <alvarez_william@outlook.com>
2021-11-20 00:27:32 -0800

    cleanup pickdest code

--- screens/Authentication/pickDestinations.js
+++ screens/Authentication/pickDestinations.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from "styled-components/native";
 import { View, TextInput, Dimensions, StyleSheet, Text, Pressable, TouchableOpacity, ImageBackground } from 'react-native';
 import {
@@ -27,32 +27,6 @@ import WhiteButton from '../../comps/Global/whiteButton.js';
 
 import { Video, AVPlaybackStatus } from 'expo-av';
 
-//put bus on its own component ting, make position a prop and add a position absolute circle on the divider
-//back button
-/* CHECKBOX ALGORITHM
-4 return states for confirm select : home, school, work, other
-if corresponding checkbox selected = return appropriate screens
-up to 4 screens
-
-use a counter state to return 4 screens
-
-if checkbox clicked an even number of times dont add to counter
-if checkbox clicked an odd number of times add to counter
-use (if checkBoxClick % 2 === 0) ? counter +1 : nothing
-
-if no checkbox clicked, counter is still 0, add a modal that says
-"you have no routes selected, conitnue to the app anyway or nah, stay?"
-
-allocate checkbox to each page by making seperate functions if pressed odd number
-of times, make counter = whatever page its allocated to
-for example if school checkbox pressed odd number of times, make counter = 2
-asd
-skip will bring up a modal are u sure which pressing yes to will route to home page
-*/
-
-//think about using https://docs.expo.dev/versions/latest/react-native/usewindowdimensions/#width
-// turn h2 below text input into smaller h3 for consistency
-//make a bottom contaienr just fo rbutton so its in the same place every time
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
@@ -134,79 +108,70 @@ const PickDestinations = ({
         Ubuntu_700Bold_Italic,
     });
 
+    //changing heading and subheading
+    const [heading, setHeading] = useState();
+    const [subheading, setSubheading] = useState();
+
+    //counter state that increments and changes page content 4 times
     const [pageCounter, setPageCounter] = useState(0);
 
+    useEffect(()=>{
+        //rules to change heading and subheading
+        if (pageCounter === 0){
+            setHeading("Where do you live?");
+            setSubheading("Get home quick and safely! Here are some of the fastest ways home!");
+        } else if (pageCounter === 1){
+            setHeading("Where is school?");
+            setSubheading("Don't be late to class! Catch the fastest rides to school below!");
+        } else if (pageCounter === 2){
+            setHeading("Where do you work?");
+            setSubheading("Punch in to work on time! Catch these rides to help you get there faster!");
+        } else if (pageCounter === 3){
+            setHeading("Another place to go?");
+            setSubheading("Time is money! Get there faster using these rides below!");
+        }
+        RouteToApp;
+    }, [pageCounter]);
+
+    const RouteToApp = () => {
+        navigation.navigate('Home');
+    }
+
     const IncrementCount = () => {
-        if (pageCounter < 4){
+        if (pageCounter < 3){
             setPageCounter(prevState => prevState + 1);
+          } else if (pageCounter === 3){
+              RouteToApp();
           } else{
             setPageCounter(0);
           }
     }
 
+    //Back Button
     const DecrementCount = () => {
-        if (pageCounter > 4){
+        if (pageCounter > 0){
             setPageCounter(prevState => prevState - 1);
           } else{
-            setPageCounter(4);
+            setPageCounter(3);
           }
     }
 
     if (!fontsLoaded) {
         return <AppLoading />;
     } else {
-        if(pageCounter === 0){
             return <Page>
                 <ImageBackground source={require("../../assets/pickdest_bg.png")} resizeMode="cover" style={styles.image}>
                     <TopContainer>
                         <Skip>
                             <Text style={styles.text_bold_white}>Skip</Text>
                         </Skip>
-                        <BusProgressBar />
+                        <BusProgressBar busPosition="0%" circlePosition="3%"/>
                     </TopContainer>
                     <Container>
-                        <H1>Pick your transit destinations</H1>
-                        <AllTheCheckboxes>
-                            <CheckboxCont>
-                                <SignUpCheckBox />
-                                <H2>Home</H2>
-                            </CheckboxCont>
-                            <CheckboxCont>
-                                <SignUpCheckBox />
-                                <H2>School</H2>
-                            </CheckboxCont>
-                            <CheckboxCont>
-                                <SignUpCheckBox />
-                                <H2>Work</H2>
-                            </CheckboxCont>
-                            <CheckboxCont>
-                                <SignUpCheckBox />
-                                <H2>Other</H2>
-                            </CheckboxCont>
-                        </AllTheCheckboxes>
-                        <WhiteButton 
-                            text="Continue"
-                            onButtonPress={IncrementCount}
-                        />
-                    </Container>
-                </ImageBackground>
-            </Page>
-        }
-    
-        if(pageCounter === 1){
-            return <Page>
-                <ImageBackground source={require("../../assets/pickdest_bg.png")} resizeMode="cover" style={styles.image}>
-                    <TopContainer>
-                        <Skip>
-                            <Text style={styles.text_bold_white}>Skip</Text>
-                        </Skip>
-                        <BusProgressBar busPosition="22.5%" circlePosition="25%"/>
-                    </TopContainer>
-                    <Container>
-                        <H1 style={styles.text_down}>Where do you live?</H1>
+                        <H1 style={styles.text_down}>{heading}</H1>
                         <SignUpInput />
                         {/* add props and maybe think about putting these 3 in a scroll view */}
-                        <H3>Get home quick and safely! Here are some of the fastest ways home!</H3>
+                        <H3>{subheading}</H3>
                         <SignUpTransitCardScroll />
                         <WhiteButton 
                             text="Continue"
@@ -215,80 +180,7 @@ const PickDestinations = ({
                     </Container>
                 </ImageBackground>
             </Page>
-        }
-    
-        if(pageCounter === 2){
-            return <Page>
-                <ImageBackground source={require("../../assets/pickdest_bg.png")} resizeMode="cover" style={styles.image}>
-                    <TopContainer>
-                        <Skip>
-                            <Text style={styles.text_bold_white}>Skip</Text>
-                        </Skip>
-                        <BusProgressBar busPosition="45%" circlePosition="47.5%"/>
-                    </TopContainer>
-                    <Container>
-                        <H1 style={styles.text_down}>Where is school?</H1>
-                        <SignUpInput />
-                        {/* add props and maybe think about putting these 3 in a scroll view */}
-                        <H3>Don’t be late to class! Catch the fastest rides to school below!</H3>
-                        <SignUpTransitCardScroll />
-                        <WhiteButton 
-                            text="Continue"
-                            onButtonPress={IncrementCount}
-                        />
-                    </Container>
-                </ImageBackground>
-            </Page>
-        }
-    
-        if(pageCounter === 3){
-            return <Page>
-                <ImageBackground source={require("../../assets/pickdest_bg.png")} resizeMode="cover" style={styles.image}>
-                    <TopContainer>
-                        <Skip>
-                            <Text style={styles.text_bold_white}>Skip</Text>
-                        </Skip>
-                        <BusProgressBar busPosition="67.5%" circlePosition="70%"/>
-                    </TopContainer>
-                    <Container>
-                        <H1 style={styles.text_down}>Where do you work?</H1>
-                        <SignUpInput />
-                        {/* add props and maybe think about putting these 3 in a scroll view */}
-                        <H3>Punch in to work on time! Catch these rides to help you get there faster!</H3>
-                        <SignUpTransitCardScroll />
-                        <WhiteButton 
-                            text="Continue"
-                            onButtonPress={IncrementCount}
-                        />
-                    </Container>
-                </ImageBackground>
-            </Page>
-        }
-    
-        if(pageCounter === 4){
-            return <Page>
-                <ImageBackground source={require("../../assets/pickdest_bg.png")} resizeMode="cover" style={styles.image}>
-                    <TopContainer>
-                        <Skip>
-                            <Text style={styles.text_bold_white}>Skip</Text>
-                        </Skip>
-                        <BusProgressBar busPosition="90%" circlePosition="93%"/>
-                    </TopContainer>
-                    <Container>
-                        <H1 style={styles.text_down}>Another place to go?</H1>
-                        <SignUpInput />
-                        <H3>Time is money! Get there faster using these rides below!</H3>
-                        <SignUpTransitCardScroll />
-                        <WhiteButton 
-                            text="Continue"
-                            onButtonPress={() => navigation.navigate('Home')}
-                        />
-                    </Container>
-                </ImageBackground>
-            </Page>
-        }
     }
-    
     
 }
 
