cbe6b265151ce8dc64bfb7dcda2e63aba96a7a1d
wilyyy <alvarez_william@outlook.com>
2021-11-20 16:17:07 -0800

    google places autocomplete started

--- screens/TripPlanner/mapHome.js
+++ screens/TripPlanner/mapHome.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { View, Dimensions, StyleSheet, Text, Pressable, TouchableOpacity, ImageBackground } from 'react-native';
 import styled from "styled-components/native";
 import MapView, { Marker } from 'react-native-maps';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 //styles
 import { COLORS } from '../../constants/styles.js';
@@ -30,9 +31,42 @@ const Container = styled.View`
     bottom: ${windowHeight / 15}px;
 `;
 
+const TopSearchBar = styled.View`
+    width: 80%;
+    position: absolute;
+    top: 7%;
+`;
+
+
 //search bar
 const MapHomeScreen = () => {
+    const [region, setRegion] = useState({
+        latitude: 49.246292,
+        longitude: -123.116226,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    })
+
     return <Page>
+        <TopSearchBar>
+            <GooglePlacesAutocomplete
+                placeholder='Start Address'
+                fetchDetails={true}
+                GooglePlacesSearchQuery={{
+                    rankby: "distance"
+                }}
+                onPress={(data, details = null) => {
+                    // 'details' is provided when fetchDetails = true
+                    console.log(data, details);
+                }}
+                query={{
+                    key: 'AIzaSyAf9zPTlsgPwAuzcHvBFAaSVvD28CCAM7U',
+                    language: 'en',
+                    components: "country:can",
+                    // types: "transit_station"
+                }}
+            />
+        </TopSearchBar>
         <MapView
             provider="google"
             initialRegion={{
@@ -42,9 +76,14 @@ const MapHomeScreen = () => {
                 longitudeDelta: 0.0421,
             }}
             style={styles.map}
+            customMapStyle={MapStyleDark}
         >
             <Marker
-
+                coordinate={{
+                    latitude: 49.246292,
+                    longitude: -123.116226,
+                }}
+                pinColor={COLORS.CAROLINABLUE}
             />
         </MapView>
         <Container>
