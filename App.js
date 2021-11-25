c03a00f3b49b93a94bed31183f4919dd00105cc4
zoelake <zoe_lake@icloud.com>
2021-11-24 22:45:37 -0800

    fixed modal bg not showing up

--- App.js
+++ App.js
@@ -47,7 +47,7 @@ export default function App() {
   return (
     <NavigationContainer>
 
-      <Stack.Navigator initialRouteName='Authentication'
+      <Stack.Navigator initialRouteName='MobileCard'
         screenOptions={{
           headerShown: false,
           animation: 'none',
